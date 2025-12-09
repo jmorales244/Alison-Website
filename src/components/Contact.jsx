98c6dd2363496f6c60a89f6a70d7403c0c9e6ded
Home PC-VS <jhjmorall@ucsc.edu>
2025-12-09 17:19:41 -0600

    adding map

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,18 @@ function Contact({ whatsappLink }) {
         <div>
           <h3>Ubicación</h3>
           <p>Tehuacán, Puebla</p>
+          <div className="footer-map">
+          <h4 className="footer-label">Encuéntranos aquí</h4>
+          <div className="footer-map-frame">
+            <iframe
+              title="Mapa de ubicación Glam Studio"
+              src="https://www.google.com/maps/embed?pb=!4v1765322003414!6m8!1m7!1szg7xMWCTDd_rkf8Dp7-dhQ!2m2!1d18.44794294594499!2d-97.40355282430195!3f183.05350741557936!4f-9.537481402370588!5f0.7820865974627469"
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
         </div>
       </div>
     </section>
